cfaed48f68eb70a71cd45012228b31b71e66b314
duffy <duffy.anderson@yahoo.com>
2025-06-24 19:37:25 -0700

    almost fixed the flippy cards, they are registered as active just need to add the action when clicked.

---
+++ resume.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (){
+    document.getElementsByClassName("flip-card-inner")[0].onclick = function(){
+        this.classList.toggle("active")
+    }
+    // for each later for each one in the list and then they dont all need different names
+})
+   
