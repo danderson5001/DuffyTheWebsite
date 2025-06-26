5081e55f0734862f71e44160b40417e93985bb89
duffy <duffy.anderson@yahoo.com>
2025-06-25 21:41:06 -0700

    hexagons and plane in the works

--- resume.js
+++ resume.js
@@ -1,12 +1,28 @@
 document.addEventListener("DOMContentLoaded", function (){
+    scheduleRandomAction();
     Array.from(document.getElementsByClassName("flip-card-inner")).forEach(element => {
         element.onclick = function(){
             this.classList.toggle("active")
         }
     });
-    // for each later for each one in the list and then they dont all need different names
+    Array.from(document.getElementsByClassName("hex-inner")).forEach(element => {
+        element.onclick = function(){
+            this.classList.toggle("active")
+        }
+    });
 })
 
+function flight(){
+
+}
+
+function scheduleRandomAction() {
+  const randomDelay =  Math.floor(Math.random() * (240000 - 20000  + 1)) + 20000 ; //20000 is about 20 seconds and 240000 is about 4 minutes
+  setTimeout(() => {
+    flight();
+    scheduleRandomAction();
+  }, randomDelay);
+}
 
 
     // document.getElementsByClassName("flip-card-inner")[0].onclick = function(){
