7de9172eb3c76994567fe7558141a508653e82d4
duffy <duffy.anderson@yahoo.com>
2025-08-05 20:14:31 -0700

    quiz logic

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,6 +1,5 @@
-
-document.getElementById("theQuiz").style.display = "none";
 document.addEventListener("DOMContentLoaded", function (){
+    document.getElementById("theQuiz").style.display = "none";
     document.getElementById("loginBtn").onclick = () => {
         let username = document.getElementById("user").value;
         let password = document.getElementById("pass").value;
@@ -11,4 +10,20 @@ document.addEventListener("DOMContentLoaded", function (){
             document.getElementById("theQuiz").style.display = "block";
         }
     };
+
+    /*** 1.tuesday 2.spiritAnimal 3.TheOnionKing 4.deathRow 5.number 6.sad 7.scary 8.actor*/
+    document.getElementById("submitQuiz").onclick = () => {
+        //pulling the user responses
+        let q1 = document.getElementById("q1") ? document.getElementById("q1").value : "";
+        let q2 = document.getElementById("q2") ? document.getElementById("q2").value : "";
+        let q3 = document.getElementById("q3") ? document.getElementById("q3").value : "";
+        let q4 = document.getElementById("q4") ? document.getElementById("q4").value : "";
+        let q5 = document.getElementById("q5") ? document.getElementById("q5").value : "";
+        let q6 = document.getElementById("q6") ? document.getElementById("q6").value : "";
+        let q7 = document.getElementById("q7") ? document.getElementById("q7").value : "";
+        let q8 = document.getElementById("q8") ? document.getElementById("q8").value : "";
+        //creating the duffy response arrays
+        let dq1 =["going up", "going hard", "going ham", "brewski", "the boys", "cocktails", "karoake", "swim"];
+        let dq2 =["hippo", "grinch", "panda", "fluffy", "","power"];
+    }
 })
