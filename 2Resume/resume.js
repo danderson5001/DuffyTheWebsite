6819dcc864fbfaf6da0cdcab3dbee8330058dfd5
duffy <duffy.anderson@yahoo.com>
2025-11-07 10:46:07 -0700

    finally figured out how I wanna do the plane logic, thanks drew.

--- 2Resume/resume.js
+++ 2Resume/resume.js
@@ -16,18 +16,12 @@ document.addEventListener("DOMContentLoaded", function (){
 
 //Flight Stuff below here ------------------------------------------------------------------------------------------------------------
 
-function create2DArray(rows, cols) {
-    const arr = new Array(Math.floor(rows));
-    for (let i = 0; i < rows; i++) {
-        arr[i] = new Array(Math.floor(cols)).fill(0);
-    }
-    return arr;
-}
-
 //Allows plane object to exist everywhere and other globals
 const plane = document.createElement('img');
-let x,y,screenx, screeny,rotation,next,  myScreen, timer;
+let x,y,screenx, screeny,rotation, myScreen, timer;
 let locationx=0, locationy=0;
+const points = new Array[2][6];
+
 
 function scheduleRandomAction() {
     if(!document.body.contains(plane)) {
@@ -61,23 +55,34 @@ function preFlight(){
     let goalx = window.innerWidth * 1.1;
     start =  Math.random()*(window.innerHeight);
     let goaly = start;
-
-    //2 dimensional array of the space between start and finish.
-    myScreen = create2DArray((window.innerHeight/10), (goalx/10));
    
     //spin logic
     rotation = 0;
-    flightPath(goalx,goaly);
+
+    /*
+        Assigns the path the plane will take by creating 
+        6 random points acrross the screeen it must hit 
+    **/
+    var sixth = window.innerWidth/6;
+    count = 0;
+    var j = 0;
+    for(var i = 0; count == 5;){
+        i += Math.random * sixth;
+        j  = Math.random * window.innerWidth;
+        points[count][0] = i;
+        points[count][1] = j;
+        count ++;
+    }
 
 //test code start
-    let str = "";
-    for(var i = 0; i < myScreen.length; i++){
-        for(var j =0;j<myScreen[0].length;j++){
-            if (myScreen[i][j] != 0 ) str += myScreen[i][j] + ", ";
-        }
-        str += "\n";
-    }
-    console.log(str +"!");
+    // let str = "";
+    // for(var i = 0; i < myScreen.length; i++){
+    //     for(var j =0;j<myScreen[0].length;j++){
+    //         if (myScreen[i][j] != 0 ) str += myScreen[i][j] + ", ";
+    //     }
+    //     str += "\n";
+    // }
+    // console.log(str +"!");
 //test code end
 }
 
@@ -89,55 +94,9 @@ function flight() {
     timer = setInterval(Run, 50);
 
     //variables for current location for fly 
-    next = 0;
     function Run() {
         flyAway();
     }
-}
-
-//this method will fill the array 
-function flightPath(goalx, goaly){
-    let pathLength = goalx*1.5;
-    let curLength = 0;
-    let stepNum = 0, xmove = 0, ymove = 0;
-    let posx = Math.floor(x/10), posy = Math.floor(y/10);
-
-    while(pathLength > curLength){
-        stepNum++;
-
-        //checks if the plane is within one of the wall , 
-        // will have ot add a "fly off  myScreen" clause when I actually do the flight 
-        if (posy < 0 || posy >= window.innerHeight/10) return myScreen;
-        if (posx < 0 || posx >= goalx/10) return myScreen;
-
-        // straightest path
-        if(y < goaly){
-            if (posy+1 < myScreen.length && posx+1 < myScreen[0].length)
-                myScreen[posy+1][posx+1] = stepNum;
-        } else if (y > goaly){
-            if (posy-1 >= 0 && posx+1 < myScreen[0].length)
-                myScreen[posy-1][posx+1] = stepNum;
-        } else {
-            if (posy >= 0 && posx+1 < myScreen[0].length)
-                myScreen[posy][posx+1] = stepNum;
-        }
-
-        // random wiggle room
-        // basic movement logic
-        xmove = Math.floor(Math.random()*3 - 1);
-        ymove = Math.floor(Math.random()*3 - 1);
-        posx = posx + xmove;
-        posy = posy + ymove;
-
-        if (posy >= 0 && posy < myScreen.length &&
-            posx >= 0 && posx < myScreen[0].length) {
-            if (myScreen[posy][posx] == 0)
-                myScreen[posy][posx] = stepNum;
-        }
-
-        curLength = stepNum*10;
-    }
-    return myScreen;
 }
 
 //in charge of actually taking the flight path that was found
@@ -172,34 +131,34 @@ function flyAway(){
         rotation = (rotation >= 360)? 0 : rotation;
     } else {
 
-         //actual movement logic
-         var moveMade = false;
-        for(let row = -1; row < 2; row++){
-            for(let col = -1; col < 2; col++){
-                if (screeny + row >= 0 &&
-                    screeny + row <  myScreen.length &&
-                    screenx + col >= 0 &&
-                    screenx + col <  myScreen[0].length){
-                    if (myScreen[screeny + row][screenx + col] != 0){ 
-                        y += row*10;
-                        x += col*10;
-                        plane.style.top = y + "px";
-                        plane.style.left = x + "px";
-                        screeny += row;
-                        screenx += col;
-                        myScreen[screeny + row][screenx + col] = 0;
-                        moveMade = true;
-                        break;
-                    }
+        // //actual movement logic
+        // var moveMade = false;
+        // for(let row = -1; row < 2; row++){
+        //     for(let col = -1; col < 2; col++){
+        //         if (screeny + row >= 0 &&
+        //             screeny + row <  myScreen.length &&
+        //             screenx + col >= 0 &&
+        //             screenx + col <  myScreen[0].length){
+        //             if (myScreen[screeny + row][screenx + col] != 0){ 
+        //                 y += row*10;
+        //                 x += col*10;
+        //                 plane.style.top = y + "px";
+        //                 plane.style.left = x + "px";
+        //                 screeny += row;
+        //                 screenx += col;
+        //                 myScreen[screeny][screenx] = 0;
+        //                 moveMade = true;
+        //                 break;
+        //             }
 
-                }
-            }//screeny + (row) >= 0 && screenx + (col) >= 0 &&
-            //screenx + (col) <  myScreen[0].length && screeny + (row) <  myScreen.length)
-        if (moveMade) break;
-        }
-        next++;
+        //         }
+        //     }//screeny + (row) >= 0 && screenx + (col) >= 0 &&
+        //     //screenx + (col) <  myScreen[0].length && screeny + (row) <  myScreen.length)
+        // if (moveMade) break;
+        // }
+        // next++;
     }
-    if(plane.style.left > window.innerWidth){
+    if(x > window.innerWidth + 5){
         plane.remove();
         clearInterval(timer);
     }
